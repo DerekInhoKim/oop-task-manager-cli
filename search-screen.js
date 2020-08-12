da43ebc25979537a9fabed9870b3a8b3766169b9
Derek <derekkiminho@gmail.com>
2020-08-12 15:37:55 -0700

    Completed functionality for 2. search screen

--- search-screen.js
+++ search-screen.js
@@ -21,17 +21,20 @@ class SearchScreen {
     console.log();
   }
 
-  printResultsUi(term) {
+  printResultsUi(term, matchingEntries) {
     console.clear();
     console.log("********************************************");
     console.log("* SEARCH RESULTS                (c) 1987   *");
     console.log("********************************************");
     console.log(); // search > to-do items text-values (both notes and task items), compare to term // if blah blah more than one, worry later // category items shouldnt be 'test fields'
     console.log("Your search matches");
+    console.log("Your search word is " + term)
     console.log();
+    console.log(matchingEntries.join('\n'));
     console.log();
   }
 
+  // let toDoEntries = ["go get milk", "something", "grab food"]
   compareSearchWords(term, toDoEntries) {
 // data => array of strings, each string one entry?
     let onlyMatchingEntries = toDoEntries.filter(function (todoEntry) { // TODO BOTTOM if todo-item has 'more than one text field', search then
@@ -40,7 +43,7 @@ class SearchScreen {
       };
     });
     // Filter to make an array of only item titles
-    return onlyMatchingEntries; // These are the item TITLES, not contents
+    return onlyMatchingEntries; // These are the item TITLES, not contents**
   };
 //  Create note page - user inputs text value, this is saved => compare to 'term' search value
 
@@ -52,8 +55,8 @@ class SearchScreen {
   show() {
     this.printUi();
     this.rl.question("> ", term => {
-      //compareSearchWords(term, /*data from */);
-      this.printResultsUi(term);
+      let matchingEntries = this.compareSearchWords(term, ["go get milk", "something", "milk", "grab food"]); //TODO connect to State**
+      this.printResultsUi(term, matchingEntries);
       this.rl.question("Enter to return to the main screen. ", () => {
         const screen = new MainScreen(this.rl, this.state);
         screen.show();
